1f42af5cc8d47c7d81a2b8df5444c2fcdeddb7aa
sadechull <linfang_613@163.com>
2016-05-06 09:13:52 +0800

    use goto to quit

--- ch4/switch.c
+++ ch4/switch.c
@@ -6,32 +6,62 @@
 
 int main()
 {
-	char ch;
+	char ch1;
 
-	switch(ch = getchar())
-	{
-		case '1':
-			printf("你的英雄是法师");
-			break;
-		case '2':
-			printf("你的英雄是圣骑士");
-			break;
-		case '3':
-			printf("你的英雄是猎人");
-			break;
-		case '4':
-			printf("你的英雄是术士");
-			break;
-		case '5': /*多个case标签可以对应一个声明*/
-		case '6':
-		case '7':
-		case '8':
-			printf("你的英雄是克苏恩");
-		break;
-		default:
-			printf("你的英雄是少主");
+	// switch(ch1 = getchar())
+	// {
+	// 	case '1':
+	// 		printf("你的英雄是法师\n");
+	// 		break;
+	// 	case '2':
+	// 		printf("你的英雄是圣骑士\n");
+	// 		break;
+	// 	case '3':
+	// 		printf("你的英雄是猎人\n");
+	// 		break;
+	// 	case '4':
+	// 		printf("你的英雄是术士\n");
+	// 		break;
+	// 	case '5': /*多个case标签可以对应一个声明*/
+	// 	case '6':
+	// 	case '7':
+	// 	case '8':
+	// 		printf("你的英雄是克苏恩\n");
+	// 	break;
+	// 	default:
+	// 		printf("你的英雄是少主\n");
+	// 	break;
 
-	}
+	// }
+
+	char ch2;
+	int lines = 0, words = 0, chars = 1;
+    while(lines < 10)//(ch2 = getchar() != EOF))
+    {
+		
+		// if( (ch2 = getchar()) == '$')
+		// 	break;
+    	switch( ch2 = getchar())
+		{
+			case '$':
+				goto quit; /*break语句只能跳出当前循环，goto可以解决这个问题*/
+			case '\n':
+				lines += 1;
+			case ' ':
+			case '\t':
+				words += 1;
+			default:
+				chars += 1;
+
+		}
+		
+
+    }
+	quit: ;
+		
+	printf("lines: %d\n", lines);
+	printf("words: %d\n", words);
+	printf("chars: %d\n", chars);
 
 	return 0;
 }
